c5743cbfedb9108ef81b36b548d19634111d9f95
fsbertoglio <83047239+fsbertoglio@users.noreply.github.com>
2023-04-09 20:31:04 -0300

    Change: Método

--- sql_scripts/create_tables.sql
+++ sql_scripts/create_tables.sql
@@ -1,157 +1,153 @@
 -- !
 DROP SCHEMA public CASCADE;
-
 CREATE SCHEMA public;
+CREATE TABLE Usuarios (
+  idUser INTEGER NOT NULL,
+  senha CHAR(32) NOT NULL,
+  nome VARCHAR(60) NOT NULL,
+  email VARCHAR(60) NOT NULL,
+  telefone VARCHAR(20),
+  PRIMARY KEY (idUser)
+);
 
-CREATE TABLE
-  Usuarios (
-    idUser INTEGER NOT NULL,
-    senha CHAR(32) NOT NULL,
-    nome VARCHAR(60) NOT NULL,
-    email VARCHAR(60) NOT NULL,
-    telefone VARCHAR(20),
-    PRIMARY KEY (idUser)
-  );
+CREATE TABLE Administradores (
+  cnpj char(14) NOT NULL,
+  PRIMARY KEY (idUser)
+) INHERITS (Usuarios);
 
-CREATE TABLE
-  Administradores (
-    cnpj char(14) NOT NULL,
-    PRIMARY KEY (idUser)
-  ) INHERITS (Usuarios);
+CREATE TABLE Clientes (
+  cep CHAR(8) NOT NULL,
+  rua VARCHAR(60) NOT NULL,
+  numeroEnd SMALLINT NOT NULL,
+  PRIMARY KEY (idUser)
+) INHERITS (Usuarios);
 
-CREATE TABLE
-  Clientes (
-    cep CHAR(8) NOT NULL,
-    rua VARCHAR(60) NOT NULL,
-    numeroEnd SMALLINT NOT NULL,
-    PRIMARY KEY (idUser)
-  ) INHERITS (Usuarios);
+CREATE TABLE Lojas (
+  idLoja INTEGER NOT NULL,
+  nome VARCHAR(60) NOT NULL UNIQUE,
+  link VARCHAR(60) NOT NULL UNIQUE,
+  telefone VARCHAR(11),
+  ramo VARCHAR(20) NOT NULL,
+  PRIMARY KEY (idLoja)
+);
 
-CREATE TABLE
-  Lojas (
-    idLoja INTEGER NOT NULL,
-    nome VARCHAR(60) NOT NULL UNIQUE,
-    link VARCHAR(60) NOT NULL UNIQUE,
-    telefone VARCHAR(11),
-    ramo VARCHAR(20) NOT NULL,
-    PRIMARY KEY (idLoja)
-  );
+CREATE TABLE Administradores_Lojas -- TABELA DE RELAÇÂO N:N
+(
+  idAdministrador INTEGER NOT NULL,
+  idLoja INTEGER NOT NULL,
+  FOREIGN KEY (idAdministrador) REFERENCES Administradores (idUser),
+  FOREIGN KEY (idLoja) REFERENCES Lojas (idLoja)
+);
 
-CREATE TABLE
-  Administradores_Lojas -- TABELA DE RELAÇÂO N:N
-  (
-    idAdministrador INTEGER NOT NULL,
-    idLoja INTEGER NOT NULL,
-    FOREIGN KEY (idAdministrador) REFERENCES Administradores (idUser),
-    FOREIGN KEY (idLoja) REFERENCES Lojas (idLoja)
-  );
-
-CREATE TABLE
-  Produtos (
-    idProduto INTEGER NOT NULL,
-    idLoja INTEGER NOT NULL,
-    nome VARCHAR(20) NOT NULL UNIQUE,
-    quantidade INTEGER NOT NULL,
-    categoria VARCHAR(20) NOT NULL,
-    valor FLOAT NOT NULL,
-    descricao TEXT NOT NULL,
-    cor VARCHAR(20),
-    tamanho VARCHAR(20),
-    FOREIGN KEY (idLoja) REFERENCES Lojas (idLoja),
-    PRIMARY KEY (idProduto, idLoja)
-  );
-
-CREATE TABLE
-  Carrinhos (
-    idCarrinho INTEGER NOT NULL,
-    idCliente INTEGER NOT NULL,
-    idLoja INTEGER NOT NULL,
-    FOREIGN KEY (idLoja) REFERENCES Lojas (idLoja),
-    FOREIGN KEY (idCliente) REFERENCES Clientes (idUser),
-    PRIMARY KEY (idCarrinho)
-  );
-
-CREATE TABLE
-  Produtos_Carrinho -- TABELA DE RELAÇÂO N:N
-  (
-    idCarrinho INTEGER NOT NULL,
-    idProduto INTEGER NOT NULL,
-    idLoja INTEGER NOT NULL,
-    quantidade INTEGER NOT NULL,
-    FOREIGN KEY (idCarrinho) REFERENCES Carrinhos (idCarrinho),
-    FOREIGN KEY (idLoja, idProduto) REFERENCES Produtos (idLoja, idProduto)
-  );
-
-CREATE TABLE
-  Compras (
-    dataHora TIMESTAMP NOT NULL,
-    valor FLOAT NOT NULL,
-    PRIMARY KEY (idCarrinho),
-    FOREIGN KEY (idCarrinho) REFERENCES Carrinhos (idCarrinho)
-  ) INHERITS (Carrinhos);
-
-CREATE TABLE
-  Metodos (
-    idMetodo INTEGER NOT NULL,
-    nome VARCHAR(20) NOT NULL,
-    parcelas INTEGER NOT NULL,
-    PRIMARY KEY (idMetodo)
-  );
-
-CREATE TABLE
-  Pagamentos (
-    idCompra INTEGER NOT NULL,
-    foiConfirmado BOOLEAN NOT NULL,
-    idMetodo INTEGER NOT NULL,
-    dataHoraPagamento TIMESTAMP NOT NULL,
-    FOREIGN KEY (idCompra) REFERENCES Compras (idCarrinho),
-    FOREIGN KEY (idMetodo) REFERENCES Metodos (idMetodo),
-    PRIMARY KEY (idCompra)
-  );
-
-CREATE TABLE
-  Avaliacoes (
-    idCliente INTEGER NOT NULL,
-    nota SMALLINT NOT NULL,
-    comentario TEXT,
-    FOREIGN KEY (idCliente) REFERENCES Clientes (idUser),
-    PRIMARY KEY (idCliente)
-  );
-
-CREATE TABLE
-  AvaliacoesLoja (
-    idLoja INTEGER NOT NULL,
-    FOREIGN KEY (idLoja) REFERENCES Lojas (idLoja),
-    PRIMARY KEY (idCliente, idLoja)
-  ) INHERITS (Avaliacoes);
-
-CREATE TABLE
-  AvaliacoesProduto (
-    idLojaProduto INTEGER NOT NULL,
-    idProduto INTEGER NOT NULL,
-    FOREIGN KEY (idProduto, idLojaProduto) REFERENCES Produtos (idProduto, idLoja),
-    PRIMARY KEY (idCliente, idLojaProduto, idProduto)
-  ) INHERITS (Avaliacoes);
-
-ALTER TABLE Metodos ADD CONSTRAINT Metodo_tipos CHECK (nome in ('Credito', 'Debito', 'Pix', 'Boleto'));
-
-ALTER TABLE Metodos
-ALTER COLUMN parcelas
-SET DEFAULT 1;
-
-ALTER TABLE Metodos ADD CONSTRAINT check_parcelas CHECK (
-  (
-    nome <> 'Credito'
-    AND parcelas = 1
-  )
-  OR (
-    nome = 'Credito'
-    AND parcelas >= 1
-    AND parcelas <= 12
+CREATE TABLE Produtos (
+  idProduto INTEGER NOT NULL,
+  idLoja INTEGER NOT NULL,
+  nome VARCHAR(30) NOT NULL,
+  quantidade INTEGER NOT NULL,
+  categoria VARCHAR(20) NOT NULL,
+  valor FLOAT NOT NULL,
+  descricao TEXT NOT NULL,
+  cor VARCHAR(20),
+  tamanho VARCHAR(20),
+  FOREIGN KEY (idLoja) REFERENCES Lojas (idLoja),
+  PRIMARY KEY (
+    idProduto,
+    idLoja
   )
 );
 
-ALTER TABLE Avaliacoes ADD CONSTRAINT check_nota CHECK (
-  nota >= 0
-  AND nota <= 5
+CREATE TABLE Carrinhos (
+  idCarrinho INTEGER NOT NULL,
+  idCliente INTEGER NOT NULL,
+  idLoja INTEGER NOT NULL,
+  FOREIGN KEY (idLoja) REFERENCES Lojas (idLoja),
+  FOREIGN KEY (idCliente) REFERENCES Clientes (idUser),
+  PRIMARY KEY (idCarrinho)
 );
+
+CREATE TABLE Produtos_Carrinho -- TABELA DE RELAÇÂO N:N
+(
+  idCarrinho INTEGER NOT NULL,
+  idProduto INTEGER NOT NULL,
+  idLoja INTEGER NOT NULL,
+  quantidade INTEGER NOT NULL,
+  FOREIGN KEY (idCarrinho) REFERENCES Carrinhos (idCarrinho),
+  FOREIGN KEY (idLoja, idProduto) REFERENCES Produtos (idLoja, idProduto)
+);
+
+CREATE TABLE Metodos (
+  idMetodo INTEGER NOT NULL,
+  nome VARCHAR(20) NOT NULL,
+  parcelas INTEGER NOT NULL,
+  PRIMARY KEY (idMetodo),
+  CONSTRAINT Metodo_tipos CHECK (
+    nome in (
+      'Credito',
+      'Debito',
+      'Pix',
+      'Boleto'
+    )
+  ),
+  CONSTRAINT check_parcelas CHECK (
+    (
+      nome <> 'Credito'
+      AND parcelas = 1
+    )
+    OR (
+      nome = 'Credito'
+      AND parcelas >= 1
+      AND parcelas <= 12
+    )
+  )
+);
+
+CREATE TABLE Compras (
+  dataHora TIMESTAMP NOT NULL,
+  valor FLOAT NOT NULL,
+  idMetodo INTEGER NOT NULL  DEFAULT 1,
+  PRIMARY KEY (idCarrinho),
+  FOREIGN KEY (idCarrinho) REFERENCES Carrinhos (idCarrinho),
+    FOREIGN KEY (idMetodo) REFERENCES Metodos (idMetodo)
+) INHERITS (Carrinhos);
+
+CREATE TABLE Pagamentos (
+  idCompra INTEGER NOT NULL,
+  foiConfirmado BOOLEAN NOT NULL,
+  dataHoraPagamento TIMESTAMP,
+  FOREIGN KEY (idCompra) REFERENCES Compras (idCarrinho),
+  PRIMARY KEY (idCompra)
+);
+
+CREATE TABLE Avaliacoes (
+  idCliente INTEGER NOT NULL,
+  nota SMALLINT NOT NULL,
+  comentario TEXT,
+  FOREIGN KEY (idCliente) REFERENCES Clientes (idUser),
+  PRIMARY KEY (idCliente), 
+  CONSTRAINT check_nota CHECK (
+    nota >= 0
+    AND nota <= 5
+  )
+);
+
+CREATE TABLE AvaliacoesLoja (
+  idLoja INTEGER NOT NULL,
+  FOREIGN KEY (idLoja) REFERENCES Lojas (idLoja),
+  PRIMARY KEY (
+    idCliente,
+    idLoja
+  )
+) INHERITS (Avaliacoes);
+CREATE TABLE AvaliacoesProduto (
+  idLojaProduto INTEGER NOT NULL,
+  idProduto INTEGER NOT NULL,
+  FOREIGN KEY (
+    idProduto,
+    idLojaProduto
+  ) REFERENCES Produtos (idProduto, idLoja),
+  PRIMARY KEY (
+    idCliente,
+    idLojaProduto,
+    idProduto
+  )
+) INHERITS (Avaliacoes);
--- sql_scripts/script_query.sql
+++ sql_scripts/script_query.sql
@@ -1,83 +1,109 @@
-select *
-from Administradores
 
-select *
-from Administradores_Lojas
-
-select *
-from Avaliacoes
-
-select *
-from AvaliacoesLoja
-
-select *
-from AvaliacoesProduto
-
-select *
-from Carrinhos
-
-select *
-from Clientes 
-
-select *
-from Compras
-
-select *
-from Lojas
-
-select *
-from  Metodos
-
-select *
-from Pagamentos
-
-select *
-from Produtos
-
-select *
-from Produtos_Carrinho
-
-select *
-from Usuarios
-
-
-select id, nome, email
-from Usuarios
-where Usuarios.id > 1
-order by nome
-
-select *
-from Administradores A join Administradores_Lojas A_L ON (A.id = A_L.idAdministrador)
-
-select *
-from Administradores A, Administradores_Lojas A_L
-
-select A.nome, A_L.idloja
-from Administradores A natural join Administradores_Lojas A_L
-
-select L.id, L.nome, A.id, A.nome
-from Administradores_Lojas A_L
-join Administradores A on A.id = A_L.idadministrador
-join Lojas L on L.id = A_L.idloja
-order by L.nome
-
-Select L.nome, COUNT (*)
-from AvaliacoesLoja AL
-join Lojas L on AL.idloja = L.id
-where AL.nota > 6
-group by L.nome
-
-SELECT L.nome, COUNT(*), MAX(AL.nota), MIN(AL.nota), AVG(AL.nota)
+-- Usando subconsultas
+	-- sem subconsulta
+SELECT L.nome, COUNT(*), MAX(AL.nota) MaiorNota, MIN(AL.nota), AVG(AL.nota)
 FROM AvaliacoesLoja AL
 JOIN Lojas L ON AL.idloja = L.idloja
-GROUP BY L.nome
-having AVG(AL.nota) >= 3
+GROUP BY L.nome;
+
+	-- com subconsulta
+SELECT DISTINCT L.nome, AL.nota
+FROM avaliacoesloja AL
+NATURAL JOIN Lojas L
+WHERE AL.nota IN (
+  SELECT MAX(AL.nota)
+  FROM AvaliacoesLoja AL
+  GROUP BY AL.idloja
+);
 
 
-select A.iduser, A.nome, L.nome
-from Administradores_Lojas AL 
-natural join Lojas L
-join Administradores A on (AL.idAdministrador = A.iduser)
-order by A.iduser
+
+-- ### CONSULTAS PARA A PARTE 2 DO TRABALHO FINAL ###
 
 
+
+-- 1. buscar administrador por id e as Lojas que ele administra
+SELECT DISTINCT A.nome nome_administrador, AL.idloja, L.nome nome_administrador
+FROM Administradores_Lojas AL
+JOIN Administradores A ON A.idUser = AL.idadministrador
+JOIN Lojas L ON L.idloja = AL.idloja
+WHERE AL.idadministrador = 2
+ORDER BY AL.idloja;
+
+-- 2. buscar loja por nome e a lista de seus administradores
+SELECT DISTINCT L.idloja, L.nome nome_loja, A.idUser, A.nome nome_administrador
+FROM Administradores_Lojas AL
+JOIN Administradores A ON A.idUser = AL.idAdministrador
+JOIN Lojas L ON L.idloja = AL.idloja
+WHERE L.nome like '%Farmacia%'
+ORDER BY A.idUser;
+
+-- 3. buscar cliente por id e mostrar as compras que ele realizou
+SELECT DISTINCT Cli.iduser, Cli.nome nome_cliente, Com.idCarrinho, Com.datahora, Com.valor, Pag.foiConfirmado, Pag.dataHoraPagamento
+FROM Clientes Cli
+JOIN Compras Com ON Com.idCliente = Cli.idUser
+JOIN Pagamentos Pag ON Com.idCarrinho = Pag.idCompra
+where Cli.idUser = 1;
+
+-- 4. buscar cliente por id e mostrar as avaliações que ele realizou
+SELECT DISTINCT Cli.iduser, Cli.nome nome_cliente, AL.idloja, L.nome nome_loja, AL.nota, AL.comentario
+FROM Clientes Cli
+JOIN AvaliacoesLoja AL ON AL.idCliente = Cli.idUser
+JOIN Lojas L ON L.idloja = AL.idloja
+WHERE Cli.idUser = 1;
+
+-- 5. Listar pagamentos pendentes de um cliente, ou seja, que não foram confirmados
+SELECT DISTINCT Cli.iduser, cli.nome nome_cliente, Pendentes.idCarrinho, Pendentes.datahora, Pendentes.valor
+  FROM Clientes Cli
+  JOIN (
+    SELECT DISTINCT Com.idCliente, Com.idCarrinho, Com.datahora, Com.valor, Pag.foiConfirmado, Pag.dataHoraPagamento
+    FROM Compras Com
+    JOIN Pagamentos Pag ON Com.idCarrinho = Pag.idCompra
+    WHERE Pag.foiConfirmado = false
+  ) AS Pendentes ON Pendentes.idCliente = Cli.idUser
+  WHERE Cli.idUser = 1;
+
+-- 6. Listar vendas de um uma loja (pode colocar filtros por mês, ano, etc)
+SELECT DISTINCT L.idloja, L.nome nome_loja, Conf.idCarrinho, Conf.datahora, Conf.valor, conf.dataHoraPagamento
+  FROM Lojas L
+  JOIN (
+    SELECT DISTINCT Com.idloja, Com.idCarrinho, Com.datahora, Com.valor, Pag.dataHoraPagamento
+    FROM Compras Com
+    JOIN Pagamentos Pag ON Com.idCarrinho = Pag.idCompra
+      WHERE Pag.foiConfirmado = true
+  ) AS conf ON Conf.idloja = L.idloja
+  WHERE L.idloja = 1;
+
+
+-- 7. Somar lucro de vendas de uma loja (pode colocar filtros por mês, ano, etc)
+
+
+-- 8. Listar clientes devedores de uma loja (que tem compras não confirmadas)
+
+
+
+
+--  VIEWS
+-- View para facilitar visão das notas atribuidas a cada loja
+
+CREATE OR REPLACE VIEW Visao_Produtos AS
+  SELECT P.idproduto, P.nome nome_produto, L.idloja, L.nome nome_loja, P.descricao, P.valor, P.quantidade, P.categoria
+    FROM produtos P
+      JOIN lojas L ON L.idloja = P.idloja;
+
+CREATE OR REPLACE VIEW Visao_Avaliacoes_Produtos AS
+    SELECT DISTINCT P.idproduto, P.idloja, avg(ap.nota) mediaAvaliacoes, COUNT(ap.nota) numAvaliacoes
+    FROM avaliacoesproduto ap
+      JOIN produtos P ON p.idproduto = ap.idproduto
+	GROUP BY P.idproduto, P.idloja;
+
+  --Visão FInal
+CREATE OR REPLACE VIEW Visao_Geral_Produtos AS
+  SELECT DISTINCT P.idloja, P.idproduto, P.nome nome_produto, L.nome nome_loja, P.descricao, P.valor, P.quantidade, P.categoria, VAP.mediaavaliacoes, VAP.numavaliacoes
+    FROM produtos P
+      JOIN lojas L ON L.idloja = P.idloja
+      JOIN Visao_Avaliacoes_Produtos VAP ON (P.idproduto = VAP.idproduto AND P.idloja = VAP.idloja);
+
+SELECT *
+FROM Visao_Geral_Produtos
+ORDER BY idloja
